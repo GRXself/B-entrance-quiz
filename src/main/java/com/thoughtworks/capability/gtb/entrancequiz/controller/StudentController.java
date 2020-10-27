5843712adb67daca110adec35d84e23b5929dd05
Ruixing Gao <ruixing.gao@thoughtworks.com>
2020-10-27 21:42:05 +0800

    Change controller return type to responseEntity.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -3,6 +3,9 @@ package com.thoughtworks.capability.gtb.entrancequiz.controller;
 import com.thoughtworks.capability.gtb.entrancequiz.model.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,9 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @CrossOrigin(origins = "http://localhost:1234")
     @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return studentService.getAllStudents();
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
     }
 }
